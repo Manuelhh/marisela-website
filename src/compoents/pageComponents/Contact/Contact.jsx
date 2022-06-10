76c50ac7d967c9701d72d9ff66004e6c4fc5d83f
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-06-10 08:49:10 -0400

    finished styling contact comp

--- src/compoents/pageComponents/Contact/Contact.jsx
+++ src/compoents/pageComponents/Contact/Contact.jsx
@@ -8,7 +8,30 @@ const Contact = () => {
         <HomeButton />
       </div>
       <div className={s.bottomContainer}>
-        <div className={s.contactFormContainer}></div>
+        <div className={s.contactFormContainer}>
+          <form action="" className={s.contactForm}>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              autocomplete="off"
+            />
+            <input
+              type="text"
+              placeholder="Subject"
+              name="subject"
+              autocomplete="off"
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              autocomplete="off"
+            />
+            <textarea name="message" rows="5" placeholder="Message"></textarea>
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
