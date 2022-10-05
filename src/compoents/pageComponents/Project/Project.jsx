dadf282b774a60aac044dcd282f78e289e64ebf7
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-10-05 15:19:35 -0400

    mogollon2

--- src/compoents/pageComponents/Project/Project.jsx
+++ src/compoents/pageComponents/Project/Project.jsx
@@ -10,60 +10,49 @@ const Project = (props) => {
       <div className={s.bottomContainer}>
         <div className={s.projectBottomContainer}>
           <div className={s.projectTextContainer}>
-            <p className={s.projectTitle}>{props.project.title}</p>
+            <p>{props.project.title}</p>
           </div>
           <div className={s.projectDescContainer}>
             <p>{props.project.description}</p>
           </div>
-          <div className={s.projectTextContainer}>
-            <p className={s.description}>{props.project.imgOneDesc}</p>
-          </div>
-          <div className={s.projectImgContainer}>
-            <img
-              className={s.projectImg}
-              src={props.project.imgOne}
-              alt="project"
-            />
-          </div>
-          <div className={s.projectTextContainer}>
-            <p className={s.description}>{props.project.imgTwoDesc}</p>
-          </div>
-          <div className={s.projectImgContainer}>
-            <img
-              className={s.projectImg}
-              src={props.project.imgTwo}
-              alt="project"
-            />
-          </div>
-          <div className={s.projectTextContainer}>
-            <p className={s.description}>{props.project.imgThreeDesc}</p>
-          </div>
-          <div className={s.projectImgContainer}>
-            <img
-              className={s.projectImg}
-              src={props.project.imgThree}
-              alt="project"
-            />
-          </div>
-          <div className={s.projectTextContainer}>
-            <p className={s.description}>{props.project.imgFourDesc}</p>
-          </div>
-          <div className={s.projectImgContainer}>
-            <img
-              className={s.projectImg}
-              src={props.project.imgFour}
-              alt="project"
-            />
-          </div>
-          <div className={s.projectTextContainer}>
-            <p className={s.description}>{props.project.imgFiveDesc}</p>
-          </div>
-          <div className={s.projectImgContainer}>
-            <img
-              className={s.projectImg}
-              src={props.project.imgFive}
-              alt="project"
-            />
+
+          <div className={s.projectImagesContainer}>
+            {" "}
+            <div className={s.projectImgContainer}>
+              <img
+                className={s.projectImg}
+                src={props.project.imgOne}
+                alt="project"
+              />
+            </div>
+            <div className={s.projectImgContainer}>
+              <img
+                className={s.projectImg}
+                src={props.project.imgTwo}
+                alt="project"
+              />
+            </div>
+            <div className={s.projectImgContainer}>
+              <img
+                className={s.projectImg}
+                src={props.project.imgThree}
+                alt="project"
+              />
+            </div>
+            <div className={s.projectImgContainer}>
+              <img
+                className={s.projectImg}
+                src={props.project.imgFour}
+                alt="project"
+              />
+            </div>
+            <div className={s.projectImgContainer}>
+              <img
+                className={s.projectImg}
+                src={props.project.imgFive}
+                alt="project"
+              />
+            </div>
           </div>
         </div>
       </div>
