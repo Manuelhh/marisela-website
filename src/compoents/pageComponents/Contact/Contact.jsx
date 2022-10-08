fbe7582ceb7de2d698f1e15d4c1f587028e6646a
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-10-08 09:15:51 -0400

    contact form email

--- src/compoents/pageComponents/Contact/Contact.jsx
+++ src/compoents/pageComponents/Contact/Contact.jsx
@@ -1,7 +1,51 @@
 import HomeButton from "../../generalComponents/HomeButton/HomeButton";
 import s from "./Contact.module.css";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const [sent, setSent] = useState(false);
+  const formRef = useRef(null);
+  const nameRef = useRef(null);
+  const subjectRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+
+  const handleUserData = (e) => {
+    e.preventDefault();
+    if (
+      nameRef.current.value === "" ||
+      subjectRef.current.value === "" ||
+      emailRef.current.value === "" ||
+      messageRef.current.value === ""
+    ) {
+      nameRef.current.focus();
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        "service_b3bqjep",
+        "template_ubefjka",
+        formRef.current,
+        "7RhgiDzoqn1bPUjZk"
+      )
+      .then(
+        (result) => {
+          console.log("AQUI!!!!!", result.text);
+          setSent(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    nameRef.current.value = "";
+    subjectRef.current.value = "";
+    emailRef.current.value = "";
+    messageRef.current.value = "";
+    nameRef.current.focus();
+  };
+
   return (
     <div className={s.contactPageContainer}>
       <div className={s.topContainer}>
@@ -9,30 +53,53 @@ const Contact = () => {
       </div>
       <div className={s.bottomContainer}>
         <div className={s.contactFormContainer}>
-          <form action="" className={s.contactForm}>
+          <form action="" className={s.contactForm} ref={formRef}>
             <input
               type="text"
               placeholder="name"
               name="name"
               autocomplete="off"
+              ref={nameRef}
             />
             <input
               type="text"
               placeholder="subject"
               name="subject"
               autocomplete="off"
+              ref={subjectRef}
             />
             <input
               type="text"
               placeholder="email"
               name="email"
               autocomplete="off"
+              ref={emailRef}
             />
-            <textarea name="message" rows="5" placeholder="message"></textarea>
-            <button>send</button>
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="message"
+              ref={messageRef}
+            ></textarea>
+            <button onClick={handleUserData}>send</button>
           </form>
         </div>
       </div>
+      {sent ? (
+        <div className={s.thanksMessage}>
+          <div>
+            message sent
+            <sup
+              className={s.thanksMessageButton}
+              onClick={() => setSent(false)}
+            >
+              close
+            </sup>
+          </div>
+        </div>
+      ) : (
+        <div className={s.thanksMessage}></div>
+      )}
     </div>
   );
 };
