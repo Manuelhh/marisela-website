11999c14aee9d79da8daa536995bd744b66af39d
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-10-09 11:59:51 -0400

    mogollon3

--- src/projectsData.js
+++ src/projectsData.js
@@ -4,7 +4,7 @@ export const projectsData = [
     path: "/lacuerpaquesomos",
     title: "La cuerpa que somos:",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.",
+      "Intervención artístico-psicoeducativa con perspectiva de género que se llevó a cabo durante el 2021 y 2022 en el Centro Artístico, Lúdico y Cultural (CALUC) del DIF Zapopan (Organismo Público Descentralizado del municipio de Zapopan). Dirigida a mujeres entre los 14 y 24 años de edad que viven en el norte del municipio de Zapopan y que se encuentran en situaciones de vulnerabilidad. La Cuerpa que Somos consistió en una serie de talleres impartidos por especialistas en los temas sexualidad, formación de redes de apoyo y defensa personal, en los que las participantes adquirieron conocimientos y herramientas útiles para el desenvolvimiento social dentro de sus contextos. Los talleres fueron un preámbulo lleno de experiencias enriquecedoras que ayudaron a que las participantes formaran un grupo cohesionado y se creara un ambiente propicio para llevar a cabo la actividad artística colaborativa, la cual giró en torno al cuerpo de las mujeres y sus significaciones. En la actividad de La Cuerpa, las participantes vistieron unas prendas blancas mientras hablamos de qué es habitar el cuerpo físico y cómo ha sido construido simbólicamente por factores sociales que afectan en la manera en que se significa de desde la dimensión individual. Las prendas blancas que tomaron la forma del cuerpo de sus portadoras, a manera de lienzo, fueron intervenidas con palabras y dibujos al mismo tiempo que se compartieron conocimientos, deseos y vivencias en un ambiente de intimidad y confianza.",
     imgOne:
       "https://res.cloudinary.com/mangud/image/upload/v1654778685/Marisela/imgOne_qbpmkj.jpg",
     imgTwo:
@@ -21,7 +21,7 @@ export const projectsData = [
     path: "/asociacion",
     title: "Asociación de dibujantes que no saben qué dibujar:",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.",
+      "Intervención artístico-educativa en la que participaron estudiantes de diversas edades de la Preparatoria 7 de la Universidad de Guadalajara. La Asociación de Dibujantes fue creada con la intención de que las y los participantes desarrollaran su creatividad y estimularan su capacidad expresiva por medio del dibujo. El único requisito para ingresar fue el gusto por dibujar, se hizo incapié en que no era importante no saber hacerlo. Cada participante creó una carpeta en la que recopilaron los ejercicios de las sesiones, donde se buscaba la introspección y el encuentro con el propio proceso creativo. Durante las sesiones se compartieron los hallazgos y las experiencias que se presentaron, así como las propuestas de nuevos ejercicios para futuros encuentros. El proyecto tuvo una duración de un año en formato presencial, en el 2018 y continuó de manera virtual en el primer semestre del 2019.",
     imgOneDesc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     imgOne:
       "https://res.cloudinary.com/mangud/image/upload/v1664984291/Marisela/Asociaci%C3%B3n_de_dibujantes_3_fxkpgv.jpg",
@@ -65,19 +65,19 @@ export const projectsData = [
     path: "/dada",
     title: "dada:",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima aliquam dolor soluta ipsam commodi fuga veritatis eum unde sed eveniet illo est exercitationem totam a, eligendi ullam ipsa sunt.",
+      "DADA (Disfrutando el Arte Desde Adentro), publicación semestral en formato de cartel plegable, es una revista-fanzine impresa y digital. Es un medio de expresión y acercamiento a la creación literaria y a las artes visuales principalmente para las y los estudiantes de la Preparatoria 7 de la Universidad de Guadalajara; busca involucrarlos junto con el personal académico y administrativo, generando vínculos que dotan de un sentido de identidad a la institución. Actualmente, la revista cuenta con diez números publicados que se han distribuido en preparatorias y centros de la UdeG. DADA, a lo largo de 9 años de vida, ha sido semillero de diversos proyectos editoriales y talleres de creación literaria. ",
     imgOneDesc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     imgOne:
-      "https://res.cloudinary.com/mangud/image/upload/v1664993413/Marisela/Screen_Shot_2022-10-05_at_2.10.04_PM_e44bxm.png",
+      "https://res.cloudinary.com/mangud/image/upload/v1665330421/Marisela/_MG_8149_qypqrc.jpg",
     imgTwoDesc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     imgTwo:
-      "https://res.cloudinary.com/mangud/image/upload/v1664993413/Marisela/Screen_Shot_2022-10-05_at_2.10.04_PM_e44bxm.png",
+      "https://res.cloudinary.com/mangud/image/upload/v1665330421/Marisela/_MG_8151_l2zdzz.jpg",
     imgThreeDesc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     imgThree:
-      "https://res.cloudinary.com/mangud/image/upload/v1664993413/Marisela/Screen_Shot_2022-10-05_at_2.10.04_PM_e44bxm.png",
+      "https://res.cloudinary.com/mangud/image/upload/v1665330423/Marisela/_MG_8150_v5jpik.jpg",
     imgFourDesc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     imgFour:
-      "https://res.cloudinary.com/mangud/image/upload/v1664993413/Marisela/Screen_Shot_2022-10-05_at_2.10.04_PM_e44bxm.png",
+      "https://res.cloudinary.com/mangud/image/upload/v1665330520/Marisela/Screen_Shot_2022-10-09_at_11.48.32_AM_nth1bg.png",
     imgFiveDesc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     imgFive:
       "https://res.cloudinary.com/mangud/image/upload/v1664993413/Marisela/Screen_Shot_2022-10-05_at_2.10.04_PM_e44bxm.png",
